d57c252d75f38b3759d9c92efe58cb4399f65e13
Simanga <simacoder@hotmail>
2024-02-22 05:19:55 +0200

    first python assignmen

---
+++ user_input.py
@@ -0,0 +1,5 @@
+name = input("Enter your name: ")
+age = int(input("enter your age: "))
+location = input("enter your location: ")
+
+print("Hello", name, "you are ", age, "years old and live in ", location)
